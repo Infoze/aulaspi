0ecae11ace3c619098db3f46216657de86c08028
José Lucas da Silva <20211204010028@ifrn.local>
2024-10-16 11:35:22 -0300

    inicializando

---
+++ src/main/java/IFRN/PI/ATIVIDADES/AtividadesApplication.java
@@ -0,0 +1,13 @@
+package IFRN.PI.ATIVIDADES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/IFRN/PI/ATIVIDADES/controles/indexControler.java
@@ -0,0 +1,13 @@
+package IFRN.PI.ATIVIDADES.controles;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexControler {
+	@RequestMapping("/")
+	public String index(){
+		System.out.println("chamou o método index");
+		return  "home";	}
+
+}
