67cd2a37785225dfcc4f9431251532ea81a118ce
José Lucas da Silva <20211204010028@ifrn.local>
2024-11-06 07:36:22 -0300

    Atualizando layout da aplicação

--- src/main/java/IFRN/PI/ATIVIDADES/controles/EventosControler.java
+++ src/main/java/IFRN/PI/ATIVIDADES/controles/EventosControler.java
@@ -1,15 +1,31 @@
 package IFRN.PI.ATIVIDADES.controles;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import IFRN.PI.ATIVIDADES.molds.Evento;
+import IFRN.PI.ATIVIDADES.repositories.EventoRepository;
 
 @Controller
 public class EventosControler {
 	
+	@Autowired 
+	private EventoRepository er;
+	
 	@RequestMapping("/eventos/form")
 	public String form(){	
-		return "FormEvento";
+		return "eventos/formEvento";
 		
 	}
-
+	@PostMapping( "/eventos")
+	public String adicionar(Evento evento) {
+		
+		System.out.println( evento);
+		er.save(evento);
+		
+		return "eventos/evento-adicionado";
+	}
 }
